014849021b2deb0375b3e00d4da49244e1c29455
Park <m05214@uengine.org>
2017-04-07 14:12:41 +0900

    작성일: 2017-04-07 작성자: 박종훈 작성내용: Iterator Pattern

---
+++ Java_DesignPattern/src/org/park/javadesignpattern/iterator/Book.java
@@ -0,0 +1,12 @@
+package org.park.javadesignpattern.iterator;
+
+public class Book {
+	private String name;
+	public Book(String name) {
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+
+}
