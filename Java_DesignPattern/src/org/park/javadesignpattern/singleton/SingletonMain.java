7bc9f61ce2b476639b5517400da309485c04512c
Park <m05214@uengine.org>
2017-05-08 13:57:06 +0900

    작성자: 박종훈 작성일: 2017-05-08 작성내용: Singleton Pattern

---
+++ Java_DesignPattern/src/org/park/javadesignpattern/singleton/SingletonMain.java
@@ -0,0 +1,27 @@
+package org.park.javadesignpattern.singleton;
+
+public class SingletonMain {
+	
+	/**
+	 * 작성자: 박종훈
+	 * 작성일: 2017-05-08
+	 * 작성내용: Singleton Pattern
+	 * 아래 내용을 이용하여  하나의 객체만 생성하고
+	 * private static Singleton singleton = new Singleton();
+	 * getSingleton()을 이용하여 생성된 객체를 전달한다.
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Start. ");
+		Singleton obj1 = Singleton.getSingleton();
+		Singleton obj2 = Singleton.getSingleton();
+		if(obj1 == obj2){
+			System.out.println("obj1과 obj2는 같은 인스턴스입니다.");
+		}else{
+			System.out.println("obj1과 obj2는 다른 인스턴스입니다.");
+		}
+		System.out.println("End. ");
+	}
+
+}
