04eb128a13c3ddbee34ccd63d8b1542a3994f04f
Park <m05214@uengine.org>
2017-05-08 18:46:27 +0900

    작성일: 2017-05-08 작성자: 박종훈 작성내용: prototype pattern 정리

--- Java_DesignPattern/src/org/park/javadesignpattern/prototype/main/PrototypeMain.java
+++ Java_DesignPattern/src/org/park/javadesignpattern/prototype/main/PrototypeMain.java
@@ -8,6 +8,24 @@ import org.park.javadesignpattern.prototype.impl.UnderlinePen;
 public class PrototypeMain {
 
 	public static void main(String[] args) {
+		/**
+		 * 
+		 * 1. Manager는 Product 인터페이스를 구현한 클래스를
+		 * 저장 및 복사한 내용을 넘겨주는 역할
+		 * 
+		 * 2. Product 인터페이스는 실행계획을 추상적인 정의만함
+		 * 
+		 * 3. Product 인터페이스를 구현한 UnderlinePen, MessageBox
+		 *    클래스는 실행계획을 구체화 한다.
+		 *    
+		 * 4. 객체 생성은 Manager 객체 내부에 있는 HashMap 자료구조에서
+		 *    key 값으로 얻어와서 복사하여 넘겨준다. 해당 객체 type은
+		 *    Product 인터페이스로 받는다.
+		 *    
+		 * 5. Product의 use() 실행은 Product 클래스를 구체화 시킨 클래스를
+		 *    실행한다.  
+		 */
+		
 		// 준비
 		Manager manager = new Manager();
 		UnderlinePen upen = new UnderlinePen('~');
