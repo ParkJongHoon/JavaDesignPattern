37537deb050bbc6ca0d67be2ad7aec30fee42f2e
ParkJongHoon <m05214@uengine.org>
2017-10-16 16:08:47 +0900

    작성일: 2017-10-12 작성자: 박종훈 작성내용: facade pattern

--- Java_DesignPattern/src/org/park/javadesignpattern/mday/chain_of_responsibility/Support.java
+++ Java_DesignPattern/src/org/park/javadesignpattern/mday/chain_of_responsibility/Support.java
@@ -1,6 +1,21 @@
 package org.park.javadesignpattern.mday.chain_of_responsibility;
 
 public abstract class Support {
+	
+	/**
+	 * 핵심 내용
+	 * 1) setNext(Support 변수) <- main()단에서 연쇄적으로 
+	 * Support chain을 설정할 수 있다. Support 변수에 넣
+	 * 
+	 * 2) support(Trouble 변수) <- 현재 설정된 Support의 하위클래스에
+	 * 따라서 resolve의 true, false 값이 결정됨 그 결과에 따라서 else if와
+	 * else로 메소드 진행이 가능하다.
+	 * 
+	 * 3) main()에서 for문을 통해서 지속적으로 support()를 돌릴 때
+	 * 상위의 Support부터 setNext로 묶여있는 가장 하위 메소드까지
+	 * 돌아간다. 그래서 해당조건에 맞는 메소드 타고 들어간다.
+	 */
+	
 	private String name;
 	private Support next;
 	public Support(String name) {
---
+++ Java_DesignPattern/src/org/park/javadesignpattern/nday/facade/main/N_Main.java
@@ -0,0 +1,12 @@
+package org.park.javadesignpattern.nday.facade.main;
+
+import org.park.javadesignpattern.nday.facade.PageMaker;
+
+public class N_Main {
+
+	public static void main(String[] args) {
+		PageMaker.makeWelcomePage("youngjin@youngjin.com", "welcome.html");
+
+	}
+
+}
