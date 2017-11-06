00a5320cab1d7736df98fc050d4f7f64e4bdfefd
ParkJongHoon <m05214@uengine.org>
2017-11-07 00:53:11 +0900

    작성일: 2017-11-07 작성자: 박종훈 작성내용: Flyweight Pattern 주석달기

--- Java_DesignPattern/src/org/park/javadesignpattern/sday/flyweight/BigString.java
+++ Java_DesignPattern/src/org/park/javadesignpattern/sday/flyweight/BigString.java
@@ -6,15 +6,16 @@ public class BigString {
 	private BigChar[] bigchars;
 	// 생성자
 	public BigString(String string){
-		bigchars = new BigChar[string.length()];
-		BigCharFactory factory = BigCharFactory.getInstance();
-		for(int i = 0; i < bigchars.length; i++){
-			bigchars[i] = factory.getBigChar(string.charAt(i));
+		bigchars = new BigChar[string.length()];	// 큰 문자 담을 컨테이너 역할
+		BigCharFactory factory = BigCharFactory.getInstance();	// 싱글턴 패턴으로 객체를 가지고 온다.
+		for(int i = 0; i < bigchars.length; i++){	// 큰 문자를 담을 컨테이너 배열에 배열의 수로 for() 돌림
+			bigchars[i] = factory.getBigChar(string.charAt(i));		// 해당 순서에 Map 객체의 value 가지고 온다.
+																	// 그러나 해당값이 없을 때 인스턴스 생성해서 넣어줌
 		}
 	}
 	// 표시
 	public void print(){
-		for(int i =0; i < bigchars.length; i++){
+		for(int i =0; i < bigchars.length; i++){	// for문의 순서에 따라서 돌림하여 큰문자 consolo 찍음 
 			bigchars[i].print();
 		}
 	}
--- Java_DesignPattern/src/org/park/javadesignpattern/sday/flyweight/main/S_Main.java
+++ Java_DesignPattern/src/org/park/javadesignpattern/sday/flyweight/main/S_Main.java
@@ -2,6 +2,19 @@ package org.park.javadesignpattern.sday.flyweight.main;
 
 import org.park.javadesignpattern.sday.flyweight.BigString;
 
+/**
+ * main 메소드 실행할 때 넣어주는 인자값(숫자로만)에 따라서
+ * 큰 문자 console에 찍힘
+ * 
+ * 전체 제어하는 클래스는 BigString임 
+ * 
+ * 이 패턴의 의도는 오브젝트(객체)를 '가볍게'하는 것에 목적이
+ * 있다. 큰 문자 객체를 사용할 때마다 매번 불러내는 것이 아니라
+ * 한번 불러낼 때 Map에 key와 value로 저장후 consolo에 큰문자를
+ * 찍고, 동일한 문자를 재호출시 Map객체에서 key로 해당 객체를
+ * 가지고와서 consolo에 출력한다. -> 쓸데없이 new 하는 것을 줄임
+ */
+
 public class S_Main {
 	public static void main(String[] args) {
 		if(args.length == 0){
