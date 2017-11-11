7b666ca0b4d2c01b3388b9b27c16777a55e540f2
ParkJongHoon <m05214@uengine.org>
2017-11-11 10:45:22 +0900

    작성일: 2017-11-11 작성자: 박종훈 작성내용: Proxy Pattern 주석달기

--- Java_DesignPattern/src/org/park/javadesignpattern/tday/proxy/PrinterProxy.java
+++ Java_DesignPattern/src/org/park/javadesignpattern/tday/proxy/PrinterProxy.java
@@ -1,5 +1,7 @@
 package org.park.javadesignpattern.tday.proxy;
 
+import tools.dynamiccreate.DynamicObjectCreater;
+
 public class PrinterProxy implements Printable {
 	private String name;			// 이름
 	private Printer real;			// '본인'
@@ -28,8 +30,12 @@ public class PrinterProxy implements Printable {
 	}
 	
 	private synchronized void realize(){	// '본인'을 생성
+		/**
+		 *	Proxy 객체가 가지고 있는 정보를 본인 객체 생성시
+		 *	넣어준다.(setter 방식으로나 construct 방식으로나) 
+		 */
 		if(real == null){
-			real = new Printer(name);
+			real = (Printer)DynamicObjectCreater.newInstanceObject("Printer", name);
 		}
 	}
 
--- Java_DesignPattern/src/org/park/javadesignpattern/tday/proxy/main/T_Main.java
+++ Java_DesignPattern/src/org/park/javadesignpattern/tday/proxy/main/T_Main.java
@@ -3,6 +3,19 @@ package org.park.javadesignpattern.tday.proxy.main;
 import org.park.javadesignpattern.tday.proxy.Printable;
 import org.park.javadesignpattern.tday.proxy.PrinterProxy;
 
+/**
+ * 본 기능( print() )과 보조 기능( setPrinterName() )으로 나누어서 생각해본다면
+ * 본 기능은 본인객체가 보조 기능은 대리인 객체(Proxy객체)가 처리한다.
+ * 그래서 먼저 보조객체가 생성 후 interface 타입으로 들어간다.
+ * 그래서 보조 기능은 Proxy객체가 하고 본 기능을 수행 할 때는
+ * 본 객체를 생성하여 진행한다.
+ * 
+ * Proxy객체에 본 기능을 호출하면 그때 본 객체를 생성하여
+ * 진행한다. 그렇기 위해서 Proxy객체 안에 본 객체를 담을 수 
+ * 있는 그릇이 필요함
+ * 
+ */
+
 public class T_Main {
 
 	public static void main(String[] args) {
