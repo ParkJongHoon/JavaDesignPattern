17da82b4b32be491b0ed9bb2c25e5d2d959dd11c
ParkJongHoon <m05214@uengine.org>
2017-11-22 17:59:21 +0900

    작성일: 2017-11-22 작성자: 박종훈 작성내용: Interpreter 패턴 주석 작성중

--- Java_DesignPattern/src/org/park/javadesignpattern/vday/Interpreter/main/V_Main.java
+++ Java_DesignPattern/src/org/park/javadesignpattern/vday/Interpreter/main/V_Main.java
@@ -3,6 +3,7 @@ package org.park.javadesignpattern.vday.Interpreter.main;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+import org.park.javadesignpattern.vday.Interpreter.ParseException;
 import org.park.javadesignpattern.vday.Interpreter.ProgramNode;
 import org.park.javadesignpattern.vday.Interpreter.V_Context;
 import org.park.javadesignpattern.vday.Interpreter.V_Node;
@@ -13,10 +14,23 @@ public class V_Main {
 		try{
 			BufferedReader reader =
 					new BufferedReader(new FileReader("program.txt"));
+			/**
+			 * program.txt 내용
+			 * 
+			 * program end
+			 * program go end
+			 * program go right go right go right go right end
+			 * program repeat 4 go right end end
+			 * program repeat 4 repeat 3  go right go left end right end end
+			 */
+			
 			String text;
 			
 			while((text = reader.readLine()) != null){
 				System.out.println("text =  \"" + text + "\"");
+				
+				// ProgramNode는 V_Node를 상속받은 구현체
+				// public void parse(V_Context context) throws ParseException 해당 메소드가 존재함
 				V_Node node = new ProgramNode();
 				node.parse(new V_Context(text));
 				System.out.println("node = " +node);
