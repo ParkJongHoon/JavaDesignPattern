6aef4228589dab92aa279365864721f69e54b5f2
ParkJongHoon <m05214@uengine.org>
2017-11-28 00:22:56 +0900

    작성일: 2017-11-11 작성자: 박종훈 작성내용: Interpreter Pattern 예제 진행중

--- Java_DesignPattern/src/org/park/javadesignpattern/vvday/Interpreter/example/VV_TurtleCanvas.java
+++ Java_DesignPattern/src/org/park/javadesignpattern/vvday/Interpreter/example/VV_TurtleCanvas.java
@@ -1,13 +1,133 @@
 package org.park.javadesignpattern.vvday.Interpreter.example;
 
-import java.awt.Canvas;
+import java.awt.*;
+
+
 
 public class VV_TurtleCanvas extends Canvas implements VV_ExecutorFactory {
+	final static int UNIT_LENGTH = 30;				// 움직일 때의 단위 길이
+	final static int DIRECTION_UP = 0;				// 위쪽 방향
+	final static int DIRECTION_RIGHT = 3;			// 오른쪽 방향
+	final static int DIRECTION_DOWN = 6;			// 아래쪽 방향
+	final static int DIRECTION_LEFT = 9;			// 왼쪽 방향
+	final static int RELATIVE_DIRECTION_RIGHT = 3; 	// 오른쪽으로 향하다.
+	final static int RELATIVE_DIRECTION_LEFT = -3;	// 왼쪽으로 향하다.
+	final static int RADIUS = 3;					// 반지름
+	private int direction = 0;
+	private Point position;
+	private VV_Executor executor;
+	public VV_TurtleCanvas(int width, int height) {
+		setSize(width, height);
+		initialize();
+	}
+	public void setExecutor(VV_Executor executor){
+		this.executor = executor;
+	}
+	void setRelativeDirection(int relativeDirection){
+		setDirection(direction + relativeDirection);
+	}
+	void setDirection(int direction){
+		if(direction < 0){
+			direction = 12 - (-direction) % 12;
+		} else{
+			direction = direction % 12;
+		}
+		this.direction = direction % 12;
+	}
+	
+	void go(int length){
+		int newx = position.x;
+		int newy = position.y;
+		switch (direction) {
+		case DIRECTION_UP:
+			newy -= length;
+			break;
+		case DIRECTION_RIGHT:
+			newy += length;
+			break;
+		case DIRECTION_DOWN:
+			newy += length;
+			break;
+		case DIRECTION_LEFT:
+			newy -= length;
+			break;
+		default:
+			break;
+		}
+		Graphics g = getGraphics();
+		if (g != null){
+			g.drawLine(position.x, position.y, newx, newy);
+			g.fillOval(newx-RADIUS, newy-RADIUS, RADIUS*2+1, RADIUS*2+1);
+		}
+		position.x = newx;
+		position.y = newy;
+	}
 
 	@Override
 	public VV_Executor createExecutor(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		if(name.equals("go")){
+			return new VV_GoExecutor(this);
+		}else if(name.equals("right")){
+			return new DirectionExecutor(this, RELATIVE_DIRECTION_RIGHT);
+		}else if(name.equals("left")){
+			return new DirectionExecutor(this, RELATIVE_DIRECTION_LEFT);
+		}else{
+			return null;
+		}
+	}
+	
+	public void initialize(){
+		Dimension size = getSize();
+		position =  new Point(size.width / 2, size.height / 2);
+		direction = 0;
+		setForeground(Color.red);
+		setBackground(Color.white);
+		Graphics g = getGraphics();
+		if(g != null){
+			g.clearRect(0, 0, size.width, size.height);
+		}
+	}
+	public void paint(Graphics g){
+		initialize();
+		if(executor != null){
+			try{
+				executor.execute();
+			}catch(VV_ExecuteException e){}
+		}
+	}
+	
+	abstract class VV_TurtleExecutor implements VV_Executor{
+		protected VV_TurtleCanvas canvas;
+		public VV_TurtleExecutor(VV_TurtleCanvas canvas){
+			this.canvas = canvas;
+		}
+	}
+	
+	class VV_GoExecutor extends VV_TurtleExecutor{
+		public VV_GoExecutor(VV_TurtleCanvas canvas){
+			super(canvas);
+		}
+
+		@Override
+		public void execute() throws VV_ExecuteException {
+			canvas.go(VV_TurtleCanvas.UNIT_LENGTH);
+			
+		}
+	}
+	
+	class DirectionExecutor extends VV_TurtleExecutor{
+		private int relativeDirection;
+		public DirectionExecutor(VV_TurtleCanvas canvas, int relativeDirection) {
+			super(canvas);
+			this.relativeDirection = relativeDirection;
+		}
+		
+		@Override
+		public void execute() throws VV_ExecuteException {
+			canvas.setRelativeDirection(relativeDirection);
+			
+		}
+		
 	}
 
 }
--- Java_DesignPattern/src/org/park/javadesignpattern/vvday/Interpreter/example/main/VV_Main.java
+++ Java_DesignPattern/src/org/park/javadesignpattern/vvday/Interpreter/example/main/VV_Main.java
@@ -1,10 +1,60 @@
 package org.park.javadesignpattern.vvday.Interpreter.example.main;
 
-public class VV_Main {
+import java.awt.*;
+import java.awt.Frame;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import org.park.javadesignpattern.vvday.Interpreter.example.VV_InterpreterFacade;
+import org.park.javadesignpattern.vvday.Interpreter.example.VV_TurtleCanvas;
 
+public class VV_Main extends Frame implements ActionListener {
+	private VV_TurtleCanvas canvas = new VV_TurtleCanvas(400, 400);
+	private VV_InterpreterFacade facade = new VV_InterpreterFacade(canvas);
+	private TextField programTextField = new TextField("program repeat 3 go right go left end end");
+
+	// 생성자
+	public VV_Main(String title) {
+		super(title);
+		canvas.setExecutor(facade);
+		setLayout(new BorderLayout());
+		programTextField.addActionListener(this);
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e){
+				System.exit(0);
+			}
+		});
+		
+		add(programTextField, BorderLayout.NORTH);
+		add(canvas, BorderLayout.CENTER);
+		pack();
+		parseAndExecute();
+		show();
 	}
+	
+	// ActionListener용
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == programTextField){
+			parseAndExecute();
+		}
+		
+	}
+	
+	private void parseAndExecute(){
+		String programText = programTextField.getText();
+		System.out.println("programText = " + programText);
+		facade.parse(programText);
+		canvas.repaint();
+	}
+	
+	public static void main(String[] args) {
+		new VV_Main("Interpreter Pattern Sample");
+	}
+
+
 
 }
